726f0aadb2d466476076ca8b14835dccdc260ef3
Sylvain Saubier (ResponSyS) <mail@systemicresponse.com>
2017-12-11 22:27:37 +0100

    This was originally written in Perl, but I've done much Perl lately and wanted a good excuse to go back to good old C so we're making a good old C program lads.

---
+++ img2b64.c
@@ -0,0 +1,76 @@
+/*
+ * =====================================================================================
+ *    Description:  
+ *
+ *        Version:  0.6
+ *        Created:  12/06/2017 09:30:51 PM
+ *       Compiler:  gcc
+ *
+ *         Author:  Sylvain S. (ResponSyS), mail@systemicresponse.com
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <regex.h>
+#include <stdnoreturn.h>
+#include <errno.h>
+
+
+#define PROGRAM_NAME 	"img2b64"
+#define VERSION 	"0.8"
+
+noreturn void
+usage( int status )
+{
+	FILE *out = status ? stderr : stdout;
+
+	fprintf( out, "%s v%s\nUsage: %s [OPTION]... {FILE}\n", 
+			PROGRAM_NAME, VERSION, PROGRAM_NAME );
+	fputs( "\
+Convert all <img> tags of HTML file 'FILE' with base64 encoding and print result\n\
+to standard output.\n\n\
+OPTIONS\n\
+    -i, --in-place [SUFFIX]\n\
+                           edit files in place (makes backup if SUFFIX supplied)\n\
+    -n, --quiet            suppress automatic printing to standard output\n\
+        --help             display this help and exit\n\
+        --version          output version information and exit\n\
+EXAMPLES\n", out );
+	fprintf( out, 
+"    %s page.html -i.old --quiet\n", PROGRAM_NAME );
+	fputs(
+"        makes backup to 'page.html.old' and silently parse 'page.html'.\n", out );
+	fputs( "\
+CONTACT\n\
+    Written by Sylvain Saubier (<http://SystemicResponse.com>)\n", out );
+	if (status)
+		fputs( "\
+    Report bugs to: <feedback@systemicresponse.com>\n", out );
+
+	exit( status );
+}
+
+int 
+main( int argc, char *argv[] )
+{
+	//regex_t re;
+	FILE * test_file1;
+	FILE * test_file2;
+
+	usage( EXIT_SUCCESS );
+	usage( 127 );
+
+	fprintf( stderr, "Opening files...\n" );
+	test_file1 = fopen( "./Tests/file1.txt", "r" );
+	test_file2 = fopen( "./Tests/file2.txt", "a" );
+
+	fprintf( stderr, "Writing files...\n" );
+	fputs( "lol\n", test_file2 );
+
+	fprintf( stderr, "Closing files...\n" );
+	fclose( test_file1 );
+	fclose( test_file2 );
+
+	return EXIT_SUCCESS;
+}				/* ----------  end of main  ---------- */
---
+++ img2b64.sh
@@ -0,0 +1,48 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#         USAGE: ./img2b64.sh --help
+# 
+#   DESCRIPTION: base64, sed, sort, uniq
+#  REQUIREMENTS: ---
+#        AUTHOR: Sylvain S. (ResponSyS), mail@systemicresponse.com
+#  ORGANIZATION: 
+#       CREATED: 11/28/2017 16:42
+#===============================================================================
+# Enables strict mode
+[[ "$DEBUG" ]] && set -o nounset -o errexit -o pipefail
+
+TEMP_B64_FILE="img2b64-$$.b64"
+trap 'rm -f --verbose "$TEMP_B64_FILE"' EXIT
+# TODO: to make it whitespace-safe use:
+#IFS=$'\n'
+
+F_IN="$1"
+pushd "$(dirname "$F_IN")" # || err
+F_IN="$(basename "$F_IN")" # || err
+echo "IN = $F_IN"
+
+# get list of all img src values
+SRC_LIST="$(sed -n "s/^.*<img\s*src=['\"]\([^\"']\+\)['\"].*$/\1/p" $F_IN | sort | uniq)"
+
+echo "Source file(s): '$SRC_LIST'"
+for IMG_PATH in $SRC_LIST ; do
+	local PLACEHOLDER=">>>${IMG_PATH}<<<"
+	base64 -w0 < "$IMG_PATH" > "$TEMP_B64_FILE"
+	head -c20 "$TEMP_B64_FILE"
+	echo
+	# can't use '/' as delimiter (troubles with /file/path) so using ',' instead
+	# SED: ARGUMENTS TOO LONG
+	# TODO: ONLY ONE WAY:
+	# 	MAKE A NEWLINE WITH A PLACEHOLDER
+	# 	REPLACE THE PLACEHOLDER WITH ED(IT)
+
+	sed -n "s,\(<img\s*src=['\"]\)${IMG_PATH}\(['\"]\),\1\n${PLACEHOLDER}\n\2,p" $F_IN
+	#printf '%s\n' "s,\(<img\s*src=['\"]\)${IMG_PATH}\(['\"]\),\1$(base64 -w0 "$IMG_PATH")\2,p"  $F_IN
+	edit $F_IN <<EOF
+/${PLACEHOLDER}/r $TEMP_B64_FILE
+1
+/${PLACEHOLDER}/d
+w
+EOF
+done
